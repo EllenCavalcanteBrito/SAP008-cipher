43630ce305db1abe688f41b7ded819d092819210
Ellen Cavalcante <srtnelly@hotmail.com>
2022-07-18 11:10:24 -0300

    Testes da function do arquivo cipher e js

--- src/cipher.js
+++
@@ -1,6 +0,0 @@
-const cipher = {
-  cipher.encode(offset, string)
-var alfabeto = "abcdefghijklmnopqrstuvxyz"
-var encode = " "
-
-export default cipher;
---
+++ src/jsCipher/index.js
@@ -0,0 +1,35 @@
+import cipher from './cipher.js';
+function clickCifrar() {
+var mensagem = document.getElementById("texto").value
+var deslocamento = document.getElementById("deslocamento").value;
+var cifra = cipher.cifragem;
+document.getElementById("texto").value = cifra;
+if (mensagem === " "){
+    alert("Por favor, escreva um texto");
+            return false;
+}
+}
+document.getElementById("cifrar").addEventListener("click", cifragem);
+
+function clickDecifrar() {
+    var cifragem = document.getElementById("texto").value
+    var deslocamento = document.getElementById("deslocamento").value;
+    var cifra = cipher.cifragem;
+    console.log (result);
+    document.getElementById("texto").value = result;
+    }
+
+/*function cifragem() {
+    var mensagem = document.getElementById("texto").value;
+    if (!mensagem.match(/[a-zA-Z]/gi)) {
+        window.alert("Caracteres e números não são aceitos")
+    } else {
+        window.alert("Ok, sua mensagem será decifrada")
+    }
+}
+document.getElementById("cifrar").addEventListener("click", cifragem);*/
+
+
+
+
+console.log(cipher);
